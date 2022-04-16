a74767edc415a11f8f0c8057d4bf1805e7e5c3cf
andrewniemann14 <76855278+andrewniemann14@users.noreply.github.com>
2022-04-16 00:22:31 -0500

    added routes

--- src/index.js
+++ src/index.js
@@ -4,17 +4,27 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home.jsx";
 import Challenge from './pages/Challenge';
 import Profile from "./pages/Profile.jsx";
-import ProfileStage from "./pages/ProfileStage.jsx";
 import reportWebVitals from './reportWebVitals';
+import Leaderboard from './pages/Leaderboard';
+import Footer from './components/Footer';
+import NavBar from './components/NavBar';
+import StageGlobal from './pages/StageGlobal';
+import ChallengePersonal from './pages/ChallengePersonal';
+import StagePersonal from './pages/StagePersonal';
 
 ReactDOM.render(
   <BrowserRouter>
+    <NavBar />
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="challenge/:id" element={<Challenge />} />
+      <Route path="leaderboard" element={<Leaderboard />} />
       <Route path="profile/:name" element={<Profile />} />
-      <Route path="profile/:name/stage/:stage" element={<ProfileStage />} />
+      <Route path="profile/:name/stage/:stage" element={<StagePersonal />} />
+      <Route path="profile/:name/challenge/:id" element={<ChallengePersonal />} />
+      <Route path="stage/:stage" element={<StageGlobal />} />
     </Routes>
+    <Footer />
   </BrowserRouter>,
   document.getElementById('root')
 );
@@ -23,3 +33,8 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+
+// React 404 on refresh:
+// https://stackoverflow.com/questions/27928372/react-router-urls-dont-work-when-refreshing-or-writing-manually
+// added a block to .htaccess and it works
