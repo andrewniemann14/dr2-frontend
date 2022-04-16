37ac6e8c5e6dbcebea4e330e2e55daf98762f011
andrewniemann14 <76855278+andrewniemann14@users.noreply.github.com>
2022-04-16 00:22:22 -0500

    basic layout components

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div className='w-full border-t-2 p-2 mt-12'>Footer</div>
+  )
+}
+
+export default Footer
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,14 @@
+import { Link } from 'react-router-dom'
+
+function NavBar() {
+  return (
+    <div className='w-full border-b-4 space-x-4 p-4 hover:shadow-inner'>
+      <Link to="/" className='left-0'>Home</Link>
+      <Link to="/leaderboard">Leaderboard</Link>
+      <Link to="/stages">Stages</Link>
+      <Link to="/profile/8ourne" className='absolute right-4'>Profile</Link> {/* head icon with name (or input) */}
+    </div>
+  )
+}
+
+export default NavBar
