65ef9cc651c6ddf0289048741f4d19e5dc8fd24a
andrewniemann14 <76855278+andrewniemann14@users.noreply.github.com>
2022-04-18 22:12:09 -0500

    points leaderboard working

--- src/pages/Leaderboard.jsx
+++ src/pages/Leaderboard.jsx
@@ -1,14 +1,41 @@
+import { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 // toggle between NAME and NATIONALITY
 
-// NAME: toggle between points and score (which is skewed by players who only did one challenge)
-// NATIONALITY: toggle between SUM(points) and SUM(score)
-
-import React from 'react'
-
 function Leaderboard() {
+  const [players, setPlayers] = useState([]);
+
+  useEffect(() => {
+    console.log('Leaderboard useEffect() running');
+    fetch(`https://data.niemann.app/dr2/index.php/players?limit=100`)
+      .then(res => res.json())
+      .then(data => setPlayers(data));
+  }, [])
   
   return (
-    <div>Leaderboard</div>
+    <div>
+      <h2 className='text-2xl text-center'>Lifetime Leaderboard</h2>
+      <table className='m-auto table-auto border-separate [border-spacing:0.75rem]'>
+        <thead>
+          <tr className='underline'>
+            <td>Name</td>
+            <td>Points</td>
+            <td>Score</td>
+          </tr>
+        </thead>
+        <tbody>
+        {players.map(e => {
+          return(
+            <tr key={`${e['name']}`} className="">
+              <td><Link to={`/profile/${e['name']}`}>{e['name']}</Link></td>
+              <td>{Math.floor(e['points'])}</td>
+              <td>{(e['points']/e['entries']).toFixed(1)}</td>
+            </tr>
+          )
+        })}
+        </tbody>
+      </table>
+    </div>
   )
 }
 
