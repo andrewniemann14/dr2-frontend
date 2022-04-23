5538381736452d382bc10367d6ba4f3ae6870639
andrewniemann14 <76855278+andrewniemann14@users.noreply.github.com>
2022-04-23 17:06:53 -0500

    housekeeping

--- src/components/HomeLink.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function HomeLink() {
-  return (
-    <span className='font-alfa-slab-one'>
-      D<span className='text-red-700'>R</span>2
-    </span>
-  )
-}
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,13 +1,12 @@
 import { Link } from 'react-router-dom'
-import HomeLink from './HomeLink'
 
 function NavBar() {
   return (
-    <div className='w-full border-b-4 space-x-4 p-4 hover:shadow-inner'>
-      <Link to="/" className='left-0'><HomeLink /></Link>
-      <Link to="/leaderboard">Leaderboard</Link>
-      <Link to="/stages">Stages</Link>
-      <Link to="/profile/8ourne" className='absolute right-4'>Profile</Link> {/* head icon with name (or input) */}
+    <div className='w-full border-b-2 space-x-4 p-4 bg-neutral-900 text-white'>
+      <Link to="/" className='left-0 hover:underline font-alfa-slab-one'>D<span className='text-red-700'>R</span><sup>2</sup></Link>
+      <Link to="/leaderboard" className='hover:text-red-600 hover:underline'>Leaderboard</Link>
+      <Link to="/stages" className='hover:text-red-600 hover:underline'>Stages</Link>
+      <Link to="/profile/8ourne" className='absolute right-4 hover:text-red-600 hover:underline'>Profile</Link> {/* head icon with name (or input) */}
     </div>
   )
 }
