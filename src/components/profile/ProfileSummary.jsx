34eb01b1835d389ecc4349aa1864717a41b70386
andrewniemann14 <76855278+andrewniemann14@users.noreply.github.com>
2022-04-27 10:02:22 -0500

    adding profile components

--- src/components/profile/ProfileSummary.jsx
+++ src/components/profile/ProfileSummary.jsx
@@ -1,11 +1,19 @@
-import ScoreRing from "./ScoreRing"
+import ScoreRing from "../ScoreRing"
+
+const tryRequire = (natString) => {
+  try {
+    return require(`../../img/flags/${natString}.png`)
+  } catch {
+    return require(`../../img/flags/eLngRestOfWorld.png`)
+  }
+}
 
 const ProfileSummary = ({name, nationality, recentScore, points}) => {
   return (
     <div className="m-auto w-fit">
       <div className='flex flex-row items-center p-2 m-auto'>
-        <img src={require(`../../img/flags/${nationality}.png`)} alt={`${nationality} flag`}
-          className='h-12 block' />
+          
+        <img src={tryRequire(nationality)} alt={`${nationality} flag`}className='h-12 block' />  
         <h1 className='text-4xl sm:text-7xl text-center ml-2 mr-8'>{name}</h1>
         
         {/* SCORE */}
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,24 +1,26 @@
 import { useEffect, useState } from 'react';
 import { Link, useParams } from 'react-router-dom';
 import ClassString from '../components/ClassString';
-import ProfileSummary from '../components/ProfileSummary';
-import TableProfileRecent from '../components/TableProfileRecent';
+import ScoreColumn from '../components/ScoreColumn'
+import ProfileSummary from '../components/profile/ProfileSummary';
+import TableProfileRecent from '../components/profile/TableProfileRecent';
 
 function Profile() {
   let { name } = useParams();
+
   const [entries, setEntries] = useState([]);
   const [nationality, setNationality] = useState('loading');
-  const [overallScore, setOverallScore] = useState(0);
+  const [recentScore, setRecentScore] = useState(0);
   const [classScores, setClassScores] = useState([]);
-
+  const [countryScores, setCountryScores] = useState([]);
   useEffect(() => {
-    console.log('Profile useEffect() running');
+    console.log('Profile useEffect() #1 running');
     fetch(`https://data.niemann.app/dr2/index.php/leaderboard?name=${name}?limit=100`)
       .then(res => res.json())
       .then(data => {
         setEntries(data);
         setNationality(data[0]['nationality']);
-        setOverallScore((data.reduce((score, datum) => {return score + datum.score}, 0) / data.length).toPrecision(3));
+        setRecentScore((data.reduce((score, datum) => {return score + datum.score}, 0) / data.length).toPrecision(3));
 
         // gets all unique values from array of objects
         let classes = [];
@@ -27,6 +29,7 @@ function Profile() {
             classes.push(e.vehicle_class);
           }
         })
+
 
         // uses array as keys for an object
         let scoredClasses = [];
@@ -40,33 +43,85 @@ function Profile() {
         // sort array of objects by a value
         setClassScores(scoredClasses.sort((a, b) => b.score - a.score))
 
-      })
+        // gets all unique values from array of objects
+        let countries = [];
+        data.forEach(e => {
+          if (!countries.some(c => JSON.stringify(c) === JSON.stringify(e.country))) {
+            countries.push(e.country);
+          }
+        })
+
+        // uses array as keys for an object
+        let scoredCountries = [];
+        countries.forEach((c, i) => {
+          let countryEntries = data.filter(e => e.country === c);
+          scoredCountries[i] = {}; // need to do this or next lines will throw 'cannot set property to undefined'
+          scoredCountries[i].country = c;
+          scoredCountries[i].score = (countryEntries.reduce((score, datum) => {return score + datum.score}, 0) / countryEntries.length).toPrecision(3);
+        })
+
+        // sort array of objects by a value
+        setCountryScores(scoredCountries.sort((a, b) => b.score - a.score))
+      }
+    )
   }, []);
 
-  try {
-  } catch {
+  const [points, setPoints] = useState(0);
+  useEffect(() => {
+    console.log('Profile useEffect() #2 running');
+    fetch(`https://data.niemann.app/dr2/index.php/players?name=${name}`)
+      .then(res => res.json())
+      .then(data => {
+        setPoints(data[0].points);
+      })
 
-  }
+  }, []);
+
 
 
   return (
-    <div className='bg-gray-700 text-white min-h-screen'>
-      <ProfileSummary name={name} overallScore={overallScore} nationality={nationality} />
-      <div className='rounded-lg bg-gray-800 w-fit m-auto shadow-lg border-2'>
+    <div className='bg-neutral-700 text-white min-h-screen'>
+      <ProfileSummary name={name} nationality={nationality} recentScore={recentScore} points={points} />
+
+      <div className="flex flex-col md:flex-row content-start">
+      <div className='rounded-lg bg-neutral-800 w-fit m-auto shadow-lg border-2'>
         <h3 className="text-4xl text-center">Recent challenges</h3>
         <TableProfileRecent name={name} entries={entries.slice(0, 10)} />
       </div>
-      <div className='rounded-lg bg-gray-800 w-fit m-auto shadow-lg border-2'>
+
+      <div className='rounded-lg bg-neutral-800 w-fit m-auto shadow-lg border-2 p-4'>
         <h3 className="text-4xl text-center">Class scores</h3>
-        {classScores.map(c => {
-          return (
-          <div key={c} className="flex flex-row">
-            <p><ClassString vehicle_class={c.class} /></p>
-            <p className='ml-4'>{c.score}</p>
-          </div>
-          )
-        })}
+        <table className='m-auto table-auto border-separate [border-spacing:0.75rem] w-full'>          
+        <tbody>
+          {classScores.map(c => {
+            return (
+            <tr key={c}>
+              <td><ClassString vehicle_class={c.class} /></td>
+              <ScoreColumn score={c.score} precision={3} />
+            </tr>
+            )
+          })}
+        </tbody>
+        </table>
       </div>
+
+      <div className='rounded-lg bg-neutral-800 w-fit m-auto shadow-lg border-2 p-4'>
+        <h3 className="text-4xl text-center">Country scores</h3>
+        <table className='m-auto table-auto border-separate [border-spacing:0.75rem] w-full'>          
+        <tbody>
+          {countryScores.map(c => {
+            return (
+            <tr key={c}>
+              <td>{c.country}</td>
+              <ScoreColumn score={c.score} precision={3} />
+            </tr>
+            )
+          })}
+        </tbody>
+        </table>
+      </div>
+      </div>
+
       <Link to={`/profile/${name}/challenges`} className='m-auto'>All challenges</Link>
     </div>
   )
