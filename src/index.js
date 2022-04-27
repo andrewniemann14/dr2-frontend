d5f16a54277fbe1601def79c092de77ccb158112
andrewniemann14 <76855278+andrewniemann14@users.noreply.github.com>
2022-04-27 10:01:00 -0500

    select player name component

--- src/index.js
+++ src/index.js
@@ -1,38 +1,36 @@
-import './index.css'
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import './index.css'
 import Home from "./pages/Home.jsx";
 import Challenge from './pages/Challenge';
 import Profile from "./pages/Profile.jsx";
-import reportWebVitals from './reportWebVitals';
 import Leaderboard from './pages/Leaderboard';
 import Footer from './components/Footer';
 import NavBar from './components/NavBar';
 import StageGlobal from './pages/StageGlobal';
 // import ChallengePersonal from './pages/ChallengePersonal';
 import StagePersonal from './pages/StagePersonal';
+import PlayerSelect from './pages/PlayerSelect';
 
-ReactDOM.render(
+const root = createRoot(document.getElementById('root'));
+root.render(
   <BrowserRouter>
     <NavBar />
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="leaderboard" element={<Leaderboard />} />
+      <Route path="identify" element={<PlayerSelect />} />
+      <Route path="profile/:name" element={<Profile />} />
       <Route path="challenge/:id" element={<Challenge />} />
       <Route path="stage/:stage" element={<StageGlobal />} />
-      <Route path="profile/:name" element={<Profile />} />
       <Route path="profile/:name/stage/:stage" element={<StagePersonal />} />
       {/* <Route path="profile/:name/challenge/:id" element={<ChallengePersonal />} /> */}
     </Routes>
     <Footer />
   </BrowserRouter>,
-  document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
 
 
 // React 404 on refresh:
---
+++ src/pages/PlayerSelect.jsx
@@ -0,0 +1,50 @@
+import { useEffect, useState } from "react"
+
+
+function PlayerSelect() {
+
+  const [players, setPlayers] = useState([])
+  const [filteredPlayers, setFilteredPlayers] = useState([' '])
+  useEffect(() => {
+    fetch('https://data.niemann.app/dr2/index.php/players?limit=10')
+      .then(res => res.json())
+      .then(data => {
+        setPlayers(data);
+        setFilteredPlayers(data);
+      })
+  }, [])
+
+  
+  const filterFunction = e => {
+    let inputText = e.target.value;
+    
+    if (inputText) {
+      const results = players.filter(p => {
+        return p.name.startsWith(inputText);
+      })
+      setFilteredPlayers(results);
+    } else {
+      setFilteredPlayers(players);
+    }
+  }
+
+  const storeFunction = e => {
+    localStorage.setItem('playerName', e.target.value);
+  }
+
+
+  return (
+    <div className='m-auto w-fit'>
+      <input type="text" onChange={filterFunction}
+        className="border-2" />
+        {filteredPlayers.length > 0 ? filteredPlayers.map(p => {
+          return (
+            <li key={p}>{p.name}</li>
+          )
+        }) : <p>no results</p>
+        }
+    </div>
+  )
+}
+
+export default PlayerSelect
