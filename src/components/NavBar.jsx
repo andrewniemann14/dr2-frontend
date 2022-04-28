9d50cc30e72204814b07c4c7a78dc3ed2c4d8028
andrewniemann14 <76855278+andrewniemann14@users.noreply.github.com>
2022-04-28 18:42:58 -0500

    added fontawesome to project

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,4 +1,6 @@
 import { Link } from 'react-router-dom'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {solid} from '@fortawesome/fontawesome-svg-core/import.macro'
 
 export default function NavBar() {
   const cookieName = localStorage.getItem('playerName');
@@ -9,7 +11,7 @@ export default function NavBar() {
       <Link to="/" className='hover:underline font-alfa-slab-one'>D<span className='text-red-700'>R</span><sup>2</sup></Link>
       <Link to="/leaderboard" className='hover:text-red-600 hover:underline'>Leaderboard</Link>
       <Link to="/stages" className='hover:text-red-600 hover:underline'>Stages</Link>
-      <Link to={"/profile"} className='hover:text-red-600 hover:underline absolute right-4'>Profile</Link> {/* head icon with name (or input) */}
+      <Link to={"/profile"} className='hover:text-red-600 hover:underline absolute right-4'><FontAwesomeIcon icon={solid('user')} /></Link> {/* head icon with name (or input) */}
     </div>
   )
 }
