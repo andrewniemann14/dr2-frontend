c7aead254499d4bab8ada9789c05461f089fcbbf
andrewniemann14 <76855278+andrewniemann14@users.noreply.github.com>
2022-04-30 12:12:04 -0500

    improved score display

--- src/components/profile/ProfileSummary.jsx
+++ src/components/profile/ProfileSummary.jsx
@@ -12,12 +12,21 @@ const tryRequire = (natString) => {
 export default function ProfileSummary({name, nationality, recentScore, points}) {
   return (
     <div className="m-auto w-fit">
-      <div className='md:flex flex-row items-center p-2 m-auto'>
-
-        <img src={tryRequire(nationality)} alt={`${nationality} flag`}className='h-8 md:h-12 block' />
-        <h1 className='text-3xl sm:text-7xl text-center ml-2 mr-8'>{name}</h1>
-
+      <div className='flex flex-col md:flex-row items-center p-2 m-auto'>
         <ScoreDisplay score={recentScore} points={points} />
+        {/* name + details div */}
+        <div className='ml-4'>
+          <h1 className='text-3xl sm:text-7xl text-center'>{name}</h1>
+          {/* details div */}
+          <div className="m-4">
+            <div className="flex">
+              <p>Nationality:</p>
+              <img src={tryRequire(nationality)} alt={`${nationality} flag`} className='h-4 w-8 ml-2 self-center' />
+            </div>
+            <p className=''>Lifetime points: {Math.floor(points)}</p>
+          </div>
+        </div>
+
 
       </div>
     </div>
--- src/components/profile/ScoreDisplay.jsx
+++ src/components/profile/ScoreDisplay.jsx
@@ -1,15 +1,20 @@
 
 import {ScoreColorStroke, ScoreColorText} from '../ScoreColors'
-import {ReactComponent as ScoreSvg} from '../../img/score-and-podium.svg'
+import {ReactComponent as ScoreSvg} from '../../img/score.svg'
+// import {ReactComponent as ScoreSvg} from '../../img/score-and-podium.svg'
 
 export default function ScoreDisplay({score, precision, points}) {
   const colorStroke = ScoreColorStroke(score);
   const colorText = ScoreColorText(score);
   return (
     <div>
-      <p className={`relative top-20 text-center text-5xl ${colorText}`}>{score}</p>
-      <ScoreSvg className={`h-48 fill-neutral-900 stroke-[4] ${colorStroke}`} />
-      <p className='relative bottom-10 text-center text-lg text-neutral-200'>{Math.floor(points)}</p>
+      <p className={`relative top-[6.5rem] text-center text-6xl ${colorText}`}>{score}</p>
+      <ScoreSvg className={`h-48 fill-neutral-900 stroke-[5] ${colorStroke}`} />
     </div>
+    // <div>
+    //   <p className={`relative top-20 text-center text-5xl ${colorText}`}>{score}</p>
+    //   <ScoreSvg className={`h-48 fill-neutral-900 stroke-[4] ${colorStroke}`} />
+    //   <p className='relative bottom-10 text-center text-lg text-neutral-200'>{Math.floor(points)}</p>
+    // </div>
   )
 }
