2aaef6dc63a065a21f51a27a391a471f96af71ac
andrewniemann14 <76855278+andrewniemann14@users.noreply.github.com>
2022-04-29 19:38:18 -0500

    added icons

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -8,10 +8,12 @@ export default function NavBar() {
 
   return (
     <div className='w-full border-b-2 space-x-4 p-4 bg-neutral-900 text-white'>
+      <>
       <Link to="/" className='hover:underline font-alfa-slab-one'>D<span className='text-red-700'>R</span><sup>2</sup></Link>
       <Link to="/leaderboard" className='hover:text-red-600 hover:underline'>Leaderboard</Link>
       <Link to="/stages" className='hover:text-red-600 hover:underline'>Stages</Link>
-      <Link to={"/profile"} className='hover:text-red-600 hover:underline absolute right-4'><FontAwesomeIcon icon={solid('user')} /></Link> {/* head icon with name (or input) */}
+      </>
+      <Link to={"/profile"} className='hover:text-red-600 hover:underline absolute right-4'><FontAwesomeIcon icon={solid('user')} className='text-xl' /></Link> {/* head icon with name (or input) */}
     </div>
   )
 }
--- src/components/home/NameChanger.jsx
+++ src/components/home/NameChanger.jsx
@@ -1,6 +1,11 @@
-import { useState } from "react"
+import { useState } from 'react'
+import { Link } from 'react-router-dom'
 
-export default function NameChanger({name, changeName}) {
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {solid} from '@fortawesome/fontawesome-svg-core/import.macro'
+// https://fontawesome.com/docs/web/use-with/react/style
+
+export default function NameChanger({name, changeName, clearName}) {
   const [inputName, setInputName] = useState(name);
 
   function handleTyping(e) {
@@ -14,8 +19,9 @@ export default function NameChanger({name, changeName}) {
 
   return (
     <form onSubmit={handleSubmit} className="flex">
-      <input type="text" name="" id="" defaultValue={name} onChange={handleTyping} className='text-black w-24' />
-      <input type="submit" value="set" />
-  </form>
+      <input type="text" name="" id="" defaultValue={name} onChange={handleTyping} className='w-24 h-8 bg-neutral-800 focus:bg-neutral-500 focus:text-black border-neutral-900 border-[1px]' />
+      <button type="submit" value="set" className='hover:text-red-600 w-8'><FontAwesomeIcon icon={solid('check')} size='lg' /></button>
+      <Link to="/identify" className='hover:text-red-600 w-8 pt-1'><FontAwesomeIcon icon={solid('magnifying-glass')} size='' /></Link>
+    </form>
   )
 }
