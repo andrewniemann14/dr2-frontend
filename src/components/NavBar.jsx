5e9837a0b6089f5b4ace7a949fa1315def5c2984
andrewniemann14 <76855278+andrewniemann14@users.noreply.github.com>
2022-04-30 12:11:34 -0500

    style change

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -9,7 +9,7 @@ export default function NavBar() {
   return (
     <div className='w-full border-b-2 space-x-4 p-4 bg-neutral-900 text-white'>
       <>
-      <Link to="/" className='hover:underline font-alfa-slab-one'>D<span className='text-red-700'>R</span><sup>2</sup></Link>
+      <Link to="/" className='font-alfa-slab-one'>D<span className='text-red-700'>R</span><sup>2</sup></Link>
       <Link to="/leaderboard" className='hover:text-red-600 hover:underline'>Leaderboard</Link>
       <Link to="/stages" className='hover:text-red-600 hover:underline'>Stages</Link>
       </>
