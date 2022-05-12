e6e0905c3438447c7c1ce2a86cd1686cc75694f5
andrewniemann14 <76855278+andrewniemann14@users.noreply.github.com>
2022-05-12 15:08:24 -0500

    progress

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,9 @@
-// https://www.freecodecamp.org/news/how-to-keep-your-footer-where-it-belongs-59c6aa05c59c/
+// TODO: make the mailto a modal contact form
 
 export default function Footer() {
   return (
-    <div className='w-full h-8 border-t-2 bg-neutral-900 text-white text-right'>Footer</div>
+    <div className='w-full h-8 border-t-2 bg-neutral-900 text-white text-right pr-4'>
+      <a href="mailto:dr2@niemann.app">Contact me</a>
+    </div>
   )
 }
