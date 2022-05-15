cd85517776ec64941cafced02996e26bd8383265
andrewniemann14 <76855278+andrewniemann14@users.noreply.github.com>
2022-05-15 17:36:17 -0500

    handles both versions

--- src/components/CountryString.js
+++ src/components/CountryString.js
@@ -1,16 +1,22 @@
 export function CountryStringLong(country) {
   switch (country) {
     case 'eArgentina':
+    case 'eLngArgentina':
       return 'Argentina'
     case 'eAustralia':
+    case 'eLngAustralia':
       return 'Australia'
     case 'eNewZealand':
+    case 'eLngNewZealand':
       return 'New Zealand'
     case 'ePoland':
+    case 'eLngPoland':
       return 'Poland'
     case 'eSpain':
+    case 'eLngSpain':
       return 'Spain'
     case 'eUsa':
+    case 'eLngUsa':
       return 'USA'
     default:
       return '--'
@@ -20,16 +26,22 @@ export function CountryStringLong(country) {
 export function CountryStringShort(country) {
   switch (country) {
     case 'eArgentina':
+    case 'eLngArgentina':
       return 'AR'
     case 'eAustralia':
+    case 'eLngAustralia':
       return 'AU'
     case 'eNewZealand':
+    case 'eLngNewZealand':
       return 'NZ'
     case 'ePoland':
+    case 'eLngPoland':
       return 'PL'
     case 'eSpain':
+    case 'eLngSpain':
       return 'ES'
     case 'eUsa':
+    case 'eLngUsa':
       return 'US'
     default:
       return '--'
