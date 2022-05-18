50ae351f5aa46f77ea08a38ff114f26f3e591bcb
andrewniemann14 <76855278+andrewniemann14@users.noreply.github.com>
2022-05-18 10:42:41 -0500

    changed stage link to state

--- src/components/home/TableChallenges.jsx
+++ src/components/home/TableChallenges.jsx
@@ -12,12 +12,12 @@ export default function TableChallenges({challenges}) {
     <tbody>
     {challenges.map(c => {
       return(
-        <tr key={`${c['id']}`} className="">
-          <td><Link to={`/challenge/${c['id']}`} className='hover:underline hover:text-red-600'>{c['id']}</Link></td>
-          <td>{c['start'].split('T')[0]}</td>
-          <td><img src={require(`../../img/flags/${c['country']}.png`)} alt={`${c['country']} flag`} className='h-4 w-8' /></td>
+        <tr key={`${c.id}`} className="">
+          <td><Link to={`/challenge/${c.id}`} className='hover:underline hover:text-red-600'>{c.id}</Link></td>
+          <td>{c.start.split('T')[0]}</td>
+          <td><img src={require(`../../img/flags/${c['country']}.png`)} alt={`${c.country} flag`} className='h-4 w-8' /></td>
           <td className=''>{ClassStringLong(c.vehicle_class)}</td>
-          <td><Link to={`/stage/${c['stage']}`} className='hover:underline hover:text-red-600'>{c['stage']}</Link></td>
+          <td><Link to='/stages' state={{stage: c.stage}} className='hover:underline hover:text-red-600'>{c.stage}</Link></td>
         </tr>
       )
     })}
